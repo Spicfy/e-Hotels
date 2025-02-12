062d255a6f8efdde81022be67a6b8435ca52285d
Spicfy <68024083+Spicfy@users.noreply.github.com>
2025-02-12 18:10:51 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,6 @@ app.get('/api/hotel-chains', async (req, res) => {
 
 
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 })
