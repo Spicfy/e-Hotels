d60757b294775edecd792d86c93d5562828b8bd3
Spicfy <68024083+Spicfy@users.noreply.github.com>
2025-02-16 17:35:59 -0500

    updated files

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 
 const pool = new Pool({
-    user: "postgres",
-    password: "B4776920$",
-    host: "localhost",
-    port: 5432, 
-    database: "ehotel"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    host:  process.env.DB_HOST,
+    port:  process.env.DB_PORT,
+    database:  process.env.DB_NAME
 });
 module.exports = pool;
